35d8be5c0edc7a1240b417910a22fa6242aa07b7
Chid <chidu93@gmail.com>
2023-08-20 22:55:03 +0530

    feat: Add summaries component

---
+++ src/pages/summaries/constants.ts
@@ -0,0 +1,5 @@
+const BASE_URL = 'http://localhost:3000';
+const BITTREX_MARKETS = `${BASE_URL}/v3/markets`;
+export const SUMMARIES_API = `${BITTREX_MARKETS}/summaries`;
+
+export const defaultErrorMessage = 'Something went wrong, please try again.';
--- src/pages/summaries/index.tsx
+++ src/pages/summaries/index.tsx
@@ -1,67 +1,107 @@
+import { useEffect, useState } from 'react';
 import { Box } from '@mui/material';
-import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
+import {
+  DataGrid,
+  GridColDef,
+  GridRowId,
+  // GridValueGetterParams
+} from '@mui/x-data-grid';
+import type {} from '@mui/x-data-grid/themeAugmentation';
+import Toast from '../../components/toast';
+import * as constants from './constants';
+
+interface Summary {
+  symbol: GridRowId;
+  high: number;
+  low: number;
+  volume: number;
+  quoteVolume: number;
+  percentChange: number;
+  updatedAt: string;
+}
+
+type SummaryItems = Array<Summary>;
 
 function Summaries() {
+  const [cryptoSummaries, setCryptoSummaries] = useState<SummaryItems>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [isError, setIsError] = useState<boolean>(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    const fetchData = async () => {
+      const url = new URL(constants.SUMMARIES_API);
+      try {
+        const response = await fetch(url);
+        const jsonData = await response.json();
+        setCryptoSummaries(jsonData);
+      } catch (e) {
+        setIsError(true);
+      }
+      setIsLoading(false);
+    };
+    fetchData();
+  }, []);
+
+  const getRowId = (row: Summary) => row.symbol;
+
   const columns: GridColDef[] = [
-    { field: 'id', headerName: 'ID', width: 90 },
     {
-      field: 'firstName',
+      field: 'symbol',
+      headerName: 'Market',
+      width: 150,
+    },
+    {
+      field: 'Price/Approx',
       headerName: 'First name',
       width: 150,
-      editable: true,
     },
     {
-      field: 'lastName',
-      headerName: 'Last name',
+      field: 'high',
+      headerName: '24h High',
       width: 150,
-      editable: true,
     },
     {
-      field: 'age',
-      headerName: 'Age',
+      field: 'low',
+      headerName: '24h Low',
+      width: 150,
+    },
+    {
+      field: 'percentChange',
+      headerName: '24h Change',
       type: 'number',
-      width: 110,
-      editable: true,
+      width: 150,
     },
     {
-      field: 'fullName',
-      headerName: 'Full name',
+      field: 'quoteVolume',
+      headerName: '24h Volume',
       description: 'This column has a value getter and is not sortable.',
-      sortable: false,
-      width: 160,
-      valueGetter: (params: GridValueGetterParams) => `${params.row.firstName || ''} ${params.row.lastName || ''}`,
+      width: 150,
+      // valueGetter: (params: GridValueGetterParams) => `${params.row.firstName || ''} ${params.row.lastName || ''}`,
     },
-  ];
-
-  const rows = [
-    { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-    { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-    { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-    { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-    { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-    { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-    { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-    { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-    { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
   ];
 
   return (
-    <Box sx={{ height: 400, width: '100%' }}>
-      <DataGrid
-        rows={rows}
-        columns={columns}
-        initialState={{
-          pagination: {
-            paginationModel: {
-              pageSize: 5,
+    <>
+      {isError && <Toast severity='error' message={constants.defaultErrorMessage} />}
+      <Box sx={{ height: 400, width: '100%' }}>
+        <DataGrid
+          getRowId={getRowId}
+          rows={cryptoSummaries}
+          columns={columns}
+          initialState={{
+            pagination: {
+              paginationModel: {
+                pageSize: 10,
+              },
             },
-          },
-        }}
-        pageSizeOptions={[5]}
-        checkboxSelection
-        disableRowSelectionOnClick
-      />
-    </Box>
+          }}
+          pageSizeOptions={[10, 50, 100]}
+          disableRowSelectionOnClick
+          loading={isLoading}
+        />
+      </Box>
+    </>
   );
 }
 
