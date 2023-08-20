1645a12c85022e4ebf3094d221bc5531ca41a9d6
Chid <chidu93@gmail.com>
2023-08-21 00:19:31 +0530

    chore: fix App test description

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('should mount', () => {
   render(<App />);
   const AppElement = screen.getByTestId('App');
   expect(AppElement).toBeInTheDocument();
